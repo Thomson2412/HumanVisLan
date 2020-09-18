aa78fbd78110454434c4d55ffea15856fec93546
Thomson2412 <thomasm.w.fink@gmail.com>
2020-09-18 23:45:33 +0200

    Update ex3

--- lab1_OWN.R
+++ lab1_OWN.R
@@ -1,30 +1,53 @@
-library(png)
-img <- readPNG("leopard_small.png")
-image(img[,,1])
+#Load libs
+library(imager)
+library(abind)
 
-input_image <- img[,,1]
-input_test <- array(runif(25), dim = c(5, 5))
-filter_edge <- matrix(c(1,0,1, 0,0,0, -1,0,1), 3, 3)
-filter_edge_extreme <- matrix(c(-1,-1,-1, -1,8,-1, -1,-1,-1), 3, 3)
-filters_test <- list(filter_edge, filter_edge_extreme)
+img_gray <- grayscale(boats)[,,,1]
+image(img_gray)
 
-conv_with_filter <- function (input, filters){
-  for(filter in filters){
+input_image <- img_gray
+#input_test <- array(runif(25), dim = c(5, 5))
 
-    ysteps <- (nrow(input) - nrow(filter)) + 1
-    xsteps <- (ncol(input) - ncol(filter)) + 1
+filter_edge <- matrix(c(
+   1, 0, 1,
+   0, 0, 0,
+  -1, 0, 1), 3, 3)
 
-    conv_matrix <- matrix(0, nrow = xsteps, ncol = ysteps)
+filter_edge_extreme <- matrix(c(
+  -1, -1, -1,
+  -1,  8, -1,
+  -1, -1, -1), 3, 3)
 
-    for(ypos in 1:ysteps){
-      for(xpos in 1:xsteps){
-        input_submatrix <- input[xpos:(xpos + ncol(filter) - 1), ypos:(ypos + nrow(filter) - 1)]
-        conv_matrix[xpos, ypos] <-  sum(input_submatrix * filter)
+filters_matrix <- array(c(filter_edge, filter_edge_extreme), dim = c(3,3,2))
+dim(abind(filter_edge, filter_edge_extreme,rev.along=0))
+
+conv_with_filter <- function (inputs, filters){
+
+  input_amount <- tail(dim(inputs), n=1)
+  filter_amount <- tail(dim(filters), n=1)
+  ysteps <- (nrow(inputs) - nrow(filters)) + 1
+  xsteps <- (ncol(inputs) - ncol(filters)) + 1
+  result_matrix <- array(0, dim = c(ysteps, xsteps, filter_amount))
+
+  for(input_iter in 1:input_amount){
+
+    for(filter_iter in 1:filter_amount){
+      filter <- filters[,,filter_iter]
+      conv_matrix <- matrix(0, nrow = ysteps, ncol = xsteps)
+
+      for(ypos in 1:ysteps){
+        for(xpos in 1:xsteps){
+          input_submatrix <- inputs[ypos:(ypos + nrow(filter) - 1), xpos:(xpos + ncol(filter) - 1)]
+          conv_matrix[ypos, xpos] <-  sum(input_submatrix * filter)
+        }
       }
+
+      result_matrix[,,filter_iter] <- conv_matrix
     }
-    print(conv_matrix)
-    image(conv_matrix)
   }
+
+  return(result_matrix)
 }
 
-conv_with_filter(input_image, filters_test)
+result <- conv_with_filter(input_image, filters_matrix)
+image(result[,,1])
